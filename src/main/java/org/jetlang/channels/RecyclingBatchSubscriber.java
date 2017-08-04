ef3c305165f9406e7bc7df2852f7ecc849506daf
Dave Rusek <dave.rusek@gmail.com>
2017-08-04 07:44:40 -0600

    use List instead of LinkedList in declarations

--- src/main/java/org/jetlang/channels/RecyclingBatchSubscriber.java
+++ src/main/java/org/jetlang/channels/RecyclingBatchSubscriber.java
@@ -19,8 +19,8 @@ public class RecyclingBatchSubscriber<T> extends BaseSubscription<T> {
     private final Callback<List<T>> _receive;
     private final int _interval;
     private final TimeUnit _timeUnit;
-    private LinkedList<T> _pending = new LinkedList<>();
-    private LinkedList<T> _active = new LinkedList<>();
+    private List<T> _pending = new LinkedList<>();
+    private List<T> _active = new LinkedList<>();
     private final Runnable _flushRunnable;
 
     public RecyclingBatchSubscriber(Fiber queue, Callback<List<T>> receive,
@@ -67,7 +67,7 @@ public class RecyclingBatchSubscriber<T> extends BaseSubscription<T> {
     private void flush() {
         _lock.lock();
         try {
-            LinkedList<T> nowPending = _active;
+            List<T> nowPending = _active;
             _active = _pending;
             _pending = nowPending;
         } finally {
